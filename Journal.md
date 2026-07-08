d14614593dddd69141b90d6f091e514b294c1117
HenryWauzivuff <145900606+HEofOttoman@users.noreply.github.com>
2026-07-08 20:48:49 +1000

    fix: I don't know what I did but no more missing header spam
    Idk man I opened it as a new folder in the workspace

--- Journal.md
+++ Journal.md
@@ -4,3 +4,7 @@
 For my alarm, I thought of a design like a macropad, able to stand up and lay horizontally at the same time. This idea is because of the simple reason that we will be getting keyswitches as buttons.
 
 I really tried to get myself familiar with onshape, uhh never mind then.
+
+## Firmware
+8 July 2026 - I had to download platformIO because I didn't want time untracked by hackatime, and also because it's much more convenient.
+I had to deal with the continuous missing header error spam
--- firmware/platformio-lolin_c3_mini/src/alarm.ino
+++ firmware/platformio-lolin_c3_mini/src/alarm.ino
@@ -1,6 +1,7 @@
+#include <Arduino.h>
+#include <SPI.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_ST7789.h>
-#include <SPI.h>
 
 #define TFT_CS 1
 #define TFT_RST 2
@@ -11,20 +12,20 @@
 Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RST);
 
 #include "mxswitch.h"
-const int switches[2, 3, 4, 5];
+// const int switches[] = [2, 3, 4, 5];
 
-for (i=0; i < 4; i++) {
-  buzzer MxSwitch()
-}
+// for (i=0; i < 4; i++) {
+//   buzzer MxSwitch()
+// }
 
-new DateTime now;
+// new DateTime now;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
 
   tft.init(284, 76);
-  tft.setColRowStart(82, 18);
+  // tft.setColRowStart(82, 18);
   tft.setRotation(2);
   Serial.println("TFT display initialised!");
   tft.fillScreen(ST77XX_BLACK);
